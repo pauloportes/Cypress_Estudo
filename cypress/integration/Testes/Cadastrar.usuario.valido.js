c5f44ab9bfb892fb69ead1539d44c1f99480254f
paulo.portes <pauloaportes#gmail.com>
2022-01-28 19:58:15 -0300

    adicionando o teste principal (cadastro de perfil)

---
+++ cypress/integration/Testes/Cadastrar.usuario.valido.js
@@ -0,0 +1,20 @@
+/// <reference types= "cypress"/>
+
+describe('cadastro_usuario', () => {
+
+        it ('informar email novo', () => {
+            cy.visit('/')
+
+        })
+
+        it ('Preencher campos obrigatórios', () => {
+
+
+        })
+
+        it ('Finalizar o cadastro', () => {
+
+
+        })
+
+})
