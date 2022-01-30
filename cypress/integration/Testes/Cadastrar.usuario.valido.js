5e444cfd68f3bc6f1b79fc71c223c8f55d77c2e2
paulo.portes <pauloaportes#gmail.com>
2022-01-30 08:36:03 -0300

    Finalizando o teste desafio

--- cypress/integration/Testes/Cadastrar.usuario.valido.js
+++ cypress/integration/Testes/Cadastrar.usuario.valido.js
@@ -5,24 +5,39 @@ const Faker = require('faker')
 
 describe('cadastro_usuario', () => {
 
+        let usuario
+
         before(() => {
+            usuario = {email: Faker.internet.email(),
+                       name: {
+                            firstName: Faker.internet.firstName(),
+                            lastName: Faker.internet.lastName()
+                       }}
             cy.visit('?controller=authentication&back=my-account')
         })
 
+
         it ('informar email novo', () => {
             cy.get('#email_create')
-                .type(`${Faker.internet.email()}{enter}`) //só preciso usuar esse aspas simples craseado e as chaves no faker por conta do uso do enter. Poderia clicar no botão de salvar cadastro usando get e click.
+                .type(`${usuario.email}{enter}`) //só preciso usuar esse aspas simples craseado e as chaves no faker por conta do uso do enter. Poderia clicar no botão de salvar cadastro usando get e click.
         })
 
         it ('Preencher campos obrigatórios', () => {
+            cy.url().should('include', '#account-creation')
+            cy.get('#email_create').should('include.value', usuario.email)
             cy.get('#id_gender2').check()
-            cy.get('#customer_firstname').type(Faker.name.firstName())
-            cy.get('#customer_lastname').type(Faker.name.lastName())
+            cy.get('#customer_firstname').type(usuario.firstName)
+            cy.get('#customer_lastname').type(usuario.lastName)
+            cy.get('#passwd').type(Faker.internet.password())
+            cy.get('#address1').type(Faker.address.streetAddress())
+            cy.get('#city').type(Faker.address.cityName())
+            cy.get('#id_state').select(`${Faker.datatype.number({min:1, max:20})}`)
+            cy.get('#postcode').type(`${Faker.datatype.number({min:10000, max:99999})}`)
+            cy.get('#phone_mobile').type(Faker.phone.phoneNumberFormat())
         })
 
         it ('Finalizar o cadastro', () => {
-
-
+            cy.get('#submitAccount > span').click()
+            cy.get('.account > span').should('have.text', `${usuario.name.firstName} ${usuario.name.lastName}`)
         })
-
 })
